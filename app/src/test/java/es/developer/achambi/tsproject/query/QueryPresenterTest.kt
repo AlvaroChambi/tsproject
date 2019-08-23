45a3e070feb77dd4072d704fbe42d4bb20d02cce
AlvaroChambi <alvaro.chambi.campos@gmail.com>
2019-08-23 11:06:55 +0200

    Fixed unit tests

--- app/src/test/java/es/developer/achambi/tsproject/query/QueryPresenterTest.kt
+++ app/src/test/java/es/developer/achambi/tsproject/query/QueryPresenterTest.kt
@@ -2,10 +2,13 @@ package es.developer.achambi.tsproject.query
 
 import android.arch.lifecycle.Lifecycle
 import es.developer.achambi.coreframework.threading.*
+import es.developer.achambi.coreframework.ui.pagination.PaginatedBuilder
 import es.developer.achambi.coreframework.ui.pagination.PaginatedPresentation
 import es.developer.achambi.tsproject.models.QueryParams
 import es.developer.achambi.tsproject.usecase.PaginatedVehicles
 import es.developer.achambi.tsproject.usecase.VehiclesUseCase
+import es.developer.achambi.tsproject.views.builder.VehicleOverviewBuilder
+import es.developer.achambi.tsproject.views.presentation.VehicleOverviewPresentation
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -24,13 +27,17 @@ class QueryPresenterTest {
     lateinit var useCase: VehiclesUseCase
     @Mock
     lateinit var lifecycleState: Lifecycle.State
-
+    @Mock
+    lateinit var presentationBuilder: VehicleOverviewBuilder
+    @Mock
+    lateinit var paginatedBuilder: PaginatedBuilder
     lateinit var queryPresenter: QueryPresenter
     val pagePresentation = ArrayList<PaginatedPresentation>()
-
+    val presentations = ArrayList<VehicleOverviewPresentation>()
     @Before
     fun setup() {
-        queryPresenter = QueryPresenter(useCase, screen, lifecycle, executor)
+        queryPresenter = QueryPresenter(useCase, screen, lifecycle, executor,presentationBuilder,
+                paginatedBuilder)
         `when`(lifecycle.currentState).thenReturn( lifecycleState )
         `when`(lifecycleState.isAtLeast(Lifecycle.State.STARTED)).thenReturn(true)
         pagePresentation.add(PaginatedPresentation(0, false))
@@ -41,6 +48,8 @@ class QueryPresenterTest {
         val queryParams = QueryParams.Builder().build()
         val result = PaginatedVehicles()
         `when`(useCase.retrieveVehicles(queryParams, 0)).thenReturn(result)
+        `when`(presentationBuilder.build(result.data)).thenReturn(presentations)
+        `when`(paginatedBuilder.buildPageInfo(result)).thenReturn(pagePresentation)
 
         queryPresenter.performSearchSelected(queryParams, expanded = false)
 
@@ -49,7 +58,7 @@ class QueryPresenterTest {
         verify(screen, times(1)).disableSearchButton()
         verify(screen, times(1)).stopLoading()
         verify(screen, times(1)).enableSearchButton()
-        verify(screen, times(1)).displayVehicles(result.vehicles,
+        verify(screen, times(1)).displayVehicles(presentations,
                 pagePresentation)
     }
 
@@ -58,6 +67,8 @@ class QueryPresenterTest {
         val queryParams = QueryParams.Builder().build()
         val result = PaginatedVehicles()
         `when`(useCase.retrieveVehicles(queryParams, 0)).thenReturn(result)
+        `when`(presentationBuilder.build(result.data)).thenReturn(presentations)
+        `when`(paginatedBuilder.buildPageInfo(result)).thenReturn(pagePresentation)
 
         queryPresenter.performSearchSelected(queryParams, expanded = true)
 
@@ -67,7 +78,7 @@ class QueryPresenterTest {
         verify(screen, times(1)).disableSearchButton()
         verify(screen, times(1)).stopLoading()
         verify(screen, times(1)).enableSearchButton()
-        verify(screen, times(1)).displayVehicles(result.vehicles,
+        verify(screen, times(1)).displayVehicles(presentations,
                 pagePresentation)
     }
 
@@ -92,12 +103,14 @@ class QueryPresenterTest {
         val queryParams = QueryParams.Builder().build()
         val result = PaginatedVehicles()
         `when`(useCase.retrieveVehicles(queryParams, 0)).thenReturn(result)
+        `when`(presentationBuilder.build(result.data)).thenReturn(presentations)
+        `when`(paginatedBuilder.buildPageInfo(result)).thenReturn(pagePresentation)
 
         queryPresenter.queryNextPage(queryParams, 0)
 
         verify(screen, times(1)).disableSearchButton()
         verify(screen, times(1)).enableSearchButton()
-        verify(screen, times(1)).displayVehicles(result.vehicles,
+        verify(screen, times(1)).displayVehicles(presentations,
                 pagePresentation)
     }
 
@@ -108,8 +121,10 @@ class QueryPresenterTest {
         pagePresentation.clear()
         pagePresentation.add(PaginatedPresentation(0, true))
         `when`(useCase.retrieveVehicles(queryParams, 0)).thenThrow(error)
+        `when`(paginatedBuilder.buildPageInfoError()).thenReturn(pagePresentation)
 
         queryPresenter.queryNextPage(queryParams, 0)
+
         verify(screen, times(1)).disableSearchButton()
         verify(screen, times(1)).enableSearchButton()
         verify(screen, times(1)).displayNextPageError(pagePresentation)
